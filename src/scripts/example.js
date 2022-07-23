585794ea3578bbb8b1039c7fb220da915baead4f
MichaelNgCen <michaelngcen@yahoo.com>
2022-07-23 19:21:51 -0400

    animation

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -8,33 +8,71 @@ canvas.height = window.innerHeight;
 
 const gravity = 1.5;
 const standingLeft = new Image();
-standingLeft.src = './src/media/sprite/sl.png'
+standingLeft.src = './src/media/sprite/standRight.png'
+
+const walkingLeft = new Image();
+walkingLeft.src = './src/media/sprite/walkLeft.png'
+
+const walkingRight = new Image();
+walkingRight.src = './src/media/sprite/walkRight.png'
+
+const jumpRight = new Image();
+jumpRight.src = './src/media/sprite/jumpRight.png'
+
+const proneRight = new Image();
+proneRight.src = './src/media/sprite/proneRight.png'
+
+const attackRight = new Image();
+attackRight.src = './src/media/sprite/attackRight.png'
 
 class Player{
     constructor() {
         this.postion = {
-            x: 40,
-            y: 520
+            x: 30,
+            y: 590
         }
         this.velocity = {
             x: 0,
             y: 0
         }
-        this.width = 66;
-        this.height = 150;
+        this.width = 61.5;
+        this.height = 86
 
         this.image = standingLeft
-        this.frames = 0
+        this.frames = 1
+        this.sprites = {
+            stand: {
+                left: standingLeft, cropWidth: 62.5, width: 67.5, height: 86
+            },
+            walkLeft: {
+                left: walkingLeft, cropWidth: 61.8, width: 67.5, height: 86
+            },
+            walkRight: {
+                right: walkingRight, cropWidth: 61.8, width: 67.5, height: 86
+            },
+            jumpRight: {
+                right: jumpRight, cropWidth: 55, width: 67.5, height: 86  
+            },
+            proneRight: {
+                right: proneRight, cropWidth: 82, width: 50, height: 40  
+            },
+            attackRight: {
+                right: attackRight, cropWidth: 72, width: 67.5, height: 80  
+            }
+            
+        }
+        this.currentSprite = this.sprites.stand.left
+        this.currentCropWidth = 61.5
     }
     draw() {
         // ctx.fillRect(this.postion.x, this.postion.y, this.width, this.height);
-        ctx.drawImage(this.image, 68 * this.frames, 0, 68, 81,
+        ctx.drawImage(this.currentSprite, this.currentCropWidth * this.frames, 0, this.currentCropWidth, this.currentSprite.height,
             this.postion.x, this.postion.y, this.width, this.height);
     }
 
     update() {
         this.frames++
-        if (this.frames > 4) this.frames = 0;
+        if (this.frames > 9) this.frames = 1;
         this.postion.y += this.velocity.y;
         this.draw();
         if (this.postion.y + this.height + this.velocity.y <= canvas.height - 140) this.velocity.y += gravity;
@@ -100,19 +138,36 @@ animate();
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case "a":
-            console.log('left');
+            // console.log('left');
             keys.left.pressed = true;
+            player.currentSprite = player.sprites.walkLeft.left;
+            player.currentCropWidth = player.sprites.walkLeft.cropWidth 
+            player.width = player.sprites.walkLeft.width;
             break;
         case "d":
-            console.log('right');
+            // console.log('right');
             keys.right.pressed = true;
+            player.currentSprite = player.sprites.walkRight.right;
+            player.currentCropWidth = player.sprites.walkRight.cropWidth
+            player.width = player.sprites.walkRight.width
             break;
         case "w":
-            console.log('up');
+            // console.log('up');
             player.velocity.y = -20;
+            player.currentSprite = player.sprites.jumpRight.right;
+            player.currentCropWidth = player.sprites.jumpRight.cropWidth
+            player.width = player.sprites.jumpRight.width
             break;
         case "s":
-            console.log('down');
+            player.currentSprite = player.sprites.proneRight.right;
+            player.currentCropWidth = player.sprites.proneRight.cropWidth
+            player.width = player.sprites.proneRight.width
+            break;
+        case "j":
+            console.log('attack');
+            player.currentSprite = player.sprites.attackRight.right;
+            player.currentCropWidth = player.sprites.attackRight.cropWidth
+            player.width = player.sprites.attackRight.width
             break;
     }
 }) 
